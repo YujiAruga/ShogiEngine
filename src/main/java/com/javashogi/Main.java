8525296b6c44568e360f1f2ffaa7e4c945bbfc1a
YujiSepher <yujisepher430ed@gmail.com>
2025-07-29 22:11:36 +0100

    Initial Project Setup

---
+++ src/main/java/com/javashogi/Main.java
@@ -0,0 +1,12 @@
+package com.javashogi;
+
+import com.javashogi.board.Board;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        System.out.println("Shogi engine started.");
+        System.out.println(board);
+    }
+}
+
---
+++ src/main/java/com/javashogi/board/Board.java
@@ -0,0 +1,8 @@
+package com.javashogi.board;
+
+public class Board {
+    @Override
+    public String toString() {
+        return "Empty Shogi board (placeholder)";
+    }
+}
