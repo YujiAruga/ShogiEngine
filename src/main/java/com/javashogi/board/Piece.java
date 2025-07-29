c3503e622ca4c5ca1c6cdaae4ac1b54539421143
YujiSepher <yujisepher430ed@gmail.com>
2025-07-29 22:42:17 +0100

    Abstract Piece class is implmented

---
+++ src/main/java/com/javashogi/board/Piece.java
@@ -0,0 +1,20 @@
+package com.javashogi.board;
+
+public abstract class Piece {
+    protected boolean isBlack; // if this is true, player is sente which means that this piece is the first player's piece.
+    protected boolean promoted = false;
+
+    public Piece(boolean isBlack) {
+        this.isBlack = isBlack;
+    }
+
+    public boolean isPromoted() {
+        return promoted;
+    }
+
+    public void promote() {
+        promoted = true;
+    }
+
+    public abstract char getSymbol();
+}
