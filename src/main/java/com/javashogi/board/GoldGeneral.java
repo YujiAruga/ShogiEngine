100f98e59370b611e77d8bcfe17dc34641230042
YujiSepher <yujisepher430ed@gmail.com>
2025-08-10 12:07:25 +0100

    isValidMove function is added to Knight, GoldGeneral, SilverGeneral

--- src/main/java/com/javashogi/board/GoldGeneral.java
+++ src/main/java/com/javashogi/board/GoldGeneral.java
@@ -9,4 +9,40 @@ public class GoldGeneral extends Piece {
     public char getSymbol() {
         return isBlack ? 'G' : 'g';
     }
+
+    @Override
+    public boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol, Board board) {
+        if (!board.inBounds(toRow, toCol)) return false;
+        else if (fromRow == toRow && fromCol == toCol) return false;
+        else if (board.isOwnAt(toRow, toCol, isBlack)) return false;
+
+        int[][] neighbors = new int[][] {
+                {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
+        };
+
+        if (isBlack) {
+            for (int[] neighbor : neighbors) {
+                if ((neighbor[0] == 1 && neighbor[1] == -1) || (neighbor[0] == 1 && neighbor[1] == 1)) {
+                    continue;
+                }
+
+                if (fromRow + neighbor[0] == toRow && fromCol + neighbor[1] == toCol) {
+                    return true;
+                }
+            }
+        }
+        else {
+            for (int[] neighbor : neighbors) {
+                if ((neighbor[0] == -1 && neighbor[1] == -1) || (neighbor[0] == -1 && neighbor[1] == 1)) {
+                    continue;
+                }
+
+                if (fromRow + neighbor[0] == toRow && fromCol + neighbor[1] == toCol) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/javashogi/board/Knight.java
+++ src/main/java/com/javashogi/board/Knight.java
@@ -9,4 +9,24 @@ public class Knight extends Piece {
     public char getSymbol() {
         return isBlack ? 'K' : 'k';
     }
+
+    @Override
+    public boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol, Board board) {
+        if (!board.inBounds(toRow, toCol)) return false;
+        else if (fromRow == toRow && fromCol == toCol) return false;
+        else if (board.isOwnAt(toRow, toCol, isBlack)) return false;
+
+        if (isBlack) {
+            if ((toRow == fromRow - 2 && toCol == fromCol - 1) || (toRow == fromRow - 2 && toCol == fromCol + 1)) {
+                return true;
+            }
+        }
+        else {
+            if ((toRow == fromRow + 2 && toCol == fromCol - 1) || (toRow == fromRow + 2 && toCol == fromCol + 1)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
