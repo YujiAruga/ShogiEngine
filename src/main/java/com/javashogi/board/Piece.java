e9f7aef9ebf7bfff8637cd6396e6c64c95fb90fb
YujiSepher <yujisepher430ed@gmail.com>
2025-08-10 12:58:20 +0100

    Promotion logic is added

--- src/main/java/com/javashogi/board/Piece.java
+++ src/main/java/com/javashogi/board/Piece.java
@@ -23,4 +23,39 @@ public abstract class Piece {
     public abstract char getSymbol();
 
     public abstract boolean isValidMove(int fromRow, int fromCol, int toRow, int toCol, Board board);
+
+    protected boolean goldLikeMove(int fromRow, int fromCol, int toRow, int toCol, Board board) {
+        if (!board.inBounds(toRow, toCol)) return false;
+        else if (toRow == fromRow && toCol == fromCol) return false;
+        else if (board.isOwnAt(toRow, toCol, isBlack)) return false;
+
+        int[][] neighbors = new int[][] {
+                {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
+        };
+
+        if (isBlack) {
+            for (int[] neighbor : neighbors) {
+                if ((neighbor[0] == 1 && neighbor[1] == -1) || (neighbor[0] == 1 && neighbor[1] == 1)) {
+                    continue;
+                }
+
+                if (fromRow + neighbor[0] == toRow && fromCol + neighbor[1] == toCol) {
+                    return true;
+                }
+            }
+        }
+        else {
+            for (int[] neighbor : neighbors) {
+                if ((neighbor[0] == -1 && neighbor[1] == -1) || (neighbor[0] == -1 && neighbor[1] == 1)) {
+                    continue;
+                }
+
+                if (fromRow + neighbor[0] == toRow && fromCol + neighbor[1] == toCol) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
