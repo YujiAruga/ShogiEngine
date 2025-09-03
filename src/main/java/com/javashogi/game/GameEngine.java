ccf1a38d3560942fb466352bdf4e384f102907e1
YujiSepher <yujisepher430ed@gmail.com>
2025-09-03 17:33:03 +0100

    Basic turn system is added

--- src/main/java/com/javashogi/game/GameEngine.java
+++ src/main/java/com/javashogi/game/GameEngine.java
@@ -11,5 +11,26 @@ public class GameEngine {
     public boolean isBlackToMove() { return blackToMove; }
     public Board board() { return board; }
 
+    public boolean makeMove(int fromRow, int fromCol, int toRow, int toCol, boolean promoteIfAvailable) {
+        Piece piece = board.getPiece(fromRow, fromCol);
+        if (piece == null || piece.isBlack() != blackToMove) return false;
+
+        if (!board.applyMoveUnchecked(fromRow, fromCol, toRow, toCol, promoteIfAvailable)) return false;
+
+        // If this turn was Black, the next turn will be White and vice versa
+        blackToMove = !blackToMove;
+
+        return true;
+    }
+
+    public boolean drop(Class<? extends Piece> type, int row, int col) {
+        if (!board.dropUnchecked(type, row, col, blackToMove)) return false;
+
+        // If this turn was Black, the next turn will be White and vice versa
+        blackToMove = !blackToMove;
+
+        return true;
+    }
+
 
 }
