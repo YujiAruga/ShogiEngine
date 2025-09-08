1612be8fe96960bd1afc4f59a399d2160654f815
YujiSepher <yujisepher430ed@gmail.com>
2025-09-08 20:39:17 +0100

    Slight modification in GameEngine

--- src/main/java/com/javashogi/game/GameEngine.java
+++ src/main/java/com/javashogi/game/GameEngine.java
@@ -15,6 +15,13 @@ public class GameEngine {
         Piece piece = board.getPiece(fromRow, fromCol);
         if (piece == null || piece.isBlack() != blackToMove) return false;
 
+        // Simulate on a copy
+        Board temp = board.deepCopy();
+        if (!temp.applyMoveUnchecked(fromRow, fromCol, toRow, toCol, promoteIfAvailable)) return false;
+
+        // check self-check
+        if (temp.isInCheck(piece.isBlack())) return false;
+
         if (!board.applyMoveUnchecked(fromRow, fromCol, toRow, toCol, promoteIfAvailable)) return false;
 
         // If this turn was Black, the next turn will be White and vice versa
@@ -24,6 +31,13 @@ public class GameEngine {
     }
 
     public boolean drop(Class<? extends Piece> type, int row, int col) {
+        boolean isBlack = blackToMove;
+
+        // Simulate
+        Board temp = board.deepCopy();
+        if (!temp.dropUnchecked(type, row, col, isBlack)) return false;
+        // cannot drop leaving own king in check
+        if (temp.isInCheck(isBlack)) return false;
         if (!board.dropUnchecked(type, row, col, blackToMove)) return false;
 
         // If this turn was Black, the next turn will be White and vice versa
